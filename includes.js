1c2250ae79930e19a0c385fc30121f300291d979
Bogdan Ciobanu <bgdn.ciobanu@gmail.com>
2013-05-17 15:40:33 +0300

    Conditioned includeBranding + rewriten includeBranding() to write multiple files (#88)

--- includes.js
+++ includes.js
@@ -37,7 +37,8 @@ function getStaticUrl() {
 
 function includeCssAndJs() {
     includeStaticFilesInBundles();
-    includeBranding();
+    if (App.general.EXTRA_BRANDING.length){
+        includeBranding(App.general.EXTRA_BRANDING);
     includeMainEntryPoint();
     includeTemplates();
 }
@@ -121,9 +122,11 @@ function includeStaticFilesInBundles_Development() {
  *       Also, we cannot use includeCss because we need it to have an ID
  *       (#custom-css).
  */
-function includeBranding() {
-   document.write("<link rel='stylesheet' id='custom-css' " +
-                  "href='/assets/custom.css'/>");
+function includeBranding(files) {
+    for(var i=0; i < files.length;i++) {
+        document.write("<link rel='stylesheet' id='custom-css' " +
+                      "href= '" + files[i] + "'/>");
+    }
 }
 
 /*
